52a0093384978d307ecc427f4b7be92b4e6930f1
Aleksandr <strtmg@yandex.ru>
2021-05-03 15:29:35 +0700

    Add basic implementation for the Base83 algorithm

---
+++ pkg/base83/base83.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%*+,-.:;=?@[]^_{|}~"
+const base = len(chars)
+
+// Encode encodes an integer value into a base83 string of the given length.
+func Encode(val int, length int) (str string) {
+	carry := int(math.Pow(float64(base), float64(length-1)))
+	for i := 0; i < length; i++ {
+		idx := val / carry % base
+		str += string(chars[idx])
+		carry /= base
+	}
+	return str
+}
+
+// Decode decodes a base83 string into an integer value.
+func Decode(str string) (val int, err error) {
+	for _, r := range str {
+		idx := strings.IndexRune(chars, r)
+		if idx == -1 {
+			return 0, fmt.Errorf("base83: invalid input %v", r)
+		}
+		val = val*base + idx
+	}
+	return val, nil
+}
